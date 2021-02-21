f6b83ccda1bf0710284c1180a8a27ef2d8320244
Catherinegichina <catherinegichinaa@gmail.com>
2021-02-21 13:23:55 +0300

    first commit

---
+++ src/kay.kt
@@ -0,0 +1,21 @@
+  fun main() {var studentname = " AKULLU KAY LINDA"
+                  studentname = "shadya Obuya guard"
+                  studentname = "Ahol Deng"
+              var phonenumber = " 0788946683"
+                  phonenumber = "0788993367"
+                  phonenumber = "0778646990"
+
+              var age = 20
+                  age = 21
+                  age = 22
+                  age = 24
+               var weight = "65.6kg"
+                   weight = "60.5kg"
+                   weight = "50.3kg"
+              var citizenkenyanBoolens=("true")
+      println("citizenkenyanBoolens")
+      println("phonenumber")
+      println("age")
+      println("weight")
+
+  }
